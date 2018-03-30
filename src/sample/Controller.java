8e196adfd57cffdf59703cf04ad5b75cbee8c6de
LucasDenele <lucas.denele@isen-lille.fr>
2018-03-30 11:25:09 +0200

    JavaFX
     - Ebauche du menu
     - Test du boutton "Play Friendly Game"

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    @FXML
+    private void playFriendlyGameLaunch(ActionEvent event) {
+        System.out.println("Launch FriendlyGame");
+    }
 }
