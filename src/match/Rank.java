8b5bbfa233c0fb0c0427f74ab3e8314db4026a0c
Anthony Carlier <anthony.carlier@isen-lille.fr>
2018-04-10 17:17:17 +0200

    TXT
    Ajout de la liste des joueurs 130M et 130F

--- src/match/Rank.java
+++ src/match/Rank.java
@@ -7,4 +7,12 @@ import player.Player;
 
 public class Rank {
 	private List<Player> playersList = new ArrayList<Player>();
+
+	public List<Player> getPlayersList() {
+		return playersList;
+	}
+
+	public void setPlayersList(List<Player> playersList) {
+		this.playersList = playersList;
+	}
 }
--- src/match/Tournament.java
+++ src/match/Tournament.java
@@ -9,4 +9,22 @@ public class Tournament {
 	private List<Game> games = new ArrayList<Game>();
 	private List<Player> players = new ArrayList<Player>();
 	private Rank rankTournament;
+	public List<Game> getGames() {
+		return games;
+	}
+	public void setGames(List<Game> games) {
+		this.games = games;
+	}
+	public List<Player> getPlayers() {
+		return players;
+	}
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+	public Rank getRankTournament() {
+		return rankTournament;
+	}
+	public void setRankTournament(Rank rankTournament) {
+		this.rankTournament = rankTournament;
+	}
 }
